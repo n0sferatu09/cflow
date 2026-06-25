5c150965ec11291a2a112369a7593df0652bd5a0
n0sferatu09 <noki4ngelvim@gmail.com>
2026-06-25 16:05:49 +0900

    added the analyzer file

---
+++ src/analyzer/analyzer.hpp
@@ -0,0 +1,12 @@
+#ifndef CFLOW_ANALYZER_HPP
+#define CFLOW_ANALYZER_HPP
+
+#include <fstream>
+
+namespace analyzer {
+    class Analyzer {
+        std::ifstream file;
+    };
+}
+
+#endif //CFLOW_ANALYZER_HPP
