d861f882fa02fad180e08dc0132f4ebf8e47f869
n0sferatu09 <noki4ngelvim@gmail.com>
2026-06-26 13:26:30 +0900

    rewrote from struct types to keywords

--- src/checker/checker.hpp
+++ src/checker/checker.hpp
@@ -6,17 +6,17 @@
 #include <algorithm>
 
 namespace analyzer {
-    class Struct_Checker {
+    class KeywordChecker {
     private:
-        static constexpr std::array<std::string_view, 3> STRUCT_TYPES {
-            "enum", "struct", "union"
+        static constexpr std::array<std::string_view, 4> KEYWORDS {
+            "for", "if", "switch", "while"
         };
 
-        static constexpr const std::array<std::string_view, 3>& get_struct_types() {
-            return STRUCT_TYPES;
+        static constexpr const std::array<std::string_view, 4>& get_struct_types() {
+            return KEYWORDS;
         }
     public:
-        static bool is_struct_type(const std::string_view word) {
+        static bool is_keyword(const std::string_view word) {
             const auto& struct_types = get_struct_types();
             return std::binary_search(struct_types.begin(), struct_types.end(), word);
         }
